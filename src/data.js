2013453e481c896c19c039a7d494070cf30926e4
jessicaobi <obicjessica@gmail.com>
2025-11-06 01:33:22 -0500

    Made changes to the categories and providers as well as added standard CSS changes

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
 // Service Categories
 export const categories = [
-    { name: "Tutoring", icon: "￿", count: 15 },
-    { name: "Hair & Beauty", icon: "￿", count: 12 },
-    { name: "Design", icon: "￿", count: 8 }
+    { name: "Tutoring", icon: "📚", count: 2 },
+    { name: "Hair & Beauty", icon: "💅🏾", count: 2 },
+    { name: "Design", icon: "🧵", count: 2 }
 ]
 
 
@@ -22,6 +22,37 @@ export const providers = [
         price: "$50",
         category: "Hair & Beauty",
         image: "https://via.placeholder.com/150"
+    },
+    {
+        id: 3,
+        name: "Whitney Dion",
+        service: "Computer Science Tutoring",
+        price: "$15/hour",
+        category: "Tutoring",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        id: 4,
+        name: "Love Johnson",
+        service: "Tailoring Services",
+        price: "$30/hour",
+        category: "Design",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        id: 5,
+        name: "Naomi Forest",
+        service: "Model Training",
+        price: "$25/hour",
+        category: "Design",
+        image: "https://via.placeholder.com/150"
+    },
+    {
+        id: 6,
+        name: "Edwina Manos",
+        service: "Nails",
+        price: "$30/hour",
+        category: "Hair & Beauty",
+        image: "https://via.placeholder.com/150"
     }
-// Add 3-4 more providers
 ]
